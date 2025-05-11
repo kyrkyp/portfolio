294cc050f4d6687e34e67dad4e3952f3ddca83e7
Kyriakos Kypraios <109945212+kyrkyp@users.noreply.github.com>
2025-05-11 08:41:25 +0300

    Add pandas to add columns from csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Set the page configuration
 st.set_page_config(layout="wide")
@@ -11,10 +12,36 @@ with col1:
 with col2:
     st.title("Kypraios Kyriakos")
     content = """
-    I am a software engineer with a passion for creating innovative solutions to complex problems.
-    I love coding and I have a deep understanding of software development principles.
-    I am always eager to learn new technologies and improve my skills.
-    I am a team player and enjoy collaborating with others to achieve common goals.
-    I am currently seeking new opportunities to apply my skills and contribute to exciting projects.
+    Hi, I am Kypraios Kyriakos, a backend software engineer with a passion for programming.
+    I live in Athens, Greece and I am currently working as a software engineer at a company that specializes in web applications.
+    I have a strong background in Python and C#, and I am always eager to learn new technologies and improve my skills.
+    I have experience in building web applications using Python and C#, and I am familiar with various frameworks such as Django, Flask, and ASP.NET.
+    I am also experienced in building RESTful APIs and working with databases such as MySQL, PostgreSQL, and MongoDB.
+    I have a strong understanding of software development principles and best practices, and I am always looking for ways to improve my code and make it more efficient.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of my demo applications I have built in Python. Feel free to contact me.
+"""
+st.write(content2)
+
+# Create a list of demo applications
+col3,empty_column ,col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source] ({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source] ({row['url']})")
