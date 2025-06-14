4d9fea565d0d8d2b34666d718e4080364897835e
Kyriakos Kypraios <109945212+kyrkyp@users.noreply.github.com>
2025-06-14 04:48:38 +0300

    Send email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Enter your email address")
+    raw_message = st.text_area("Enter your message")
+    message = f"""\
+Subject: Contact from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Email sent successfully!")
---
+++ send_email.py
@@ -0,0 +1,14 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "kyriakoskyp@gmail.com"
+    password = "uonm bmok jiiw mzug"
+    receiver = "kyriakoskyp@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
